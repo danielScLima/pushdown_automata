ad7ade04fcb87490ff09553997146a0ad57bd3c8
Daniel Sucupira Lima <daniellima32@gmail.com>
2023-11-11 22:33:33 -0300

    Fazendo exemplo inicial

--- PA/main.cpp
+++ PA/main.cpp
@@ -11,7 +11,7 @@ int main()
     AutomataExecution automataExecution(PushdownAutomataSamples::get_structure_of_sample_1());
     automataExecution.draw_automata();
 
-    automataExecution.process_word("01");
+    automataExecution.process_word("000111");
     //automataExecution.test1_remove_later("00001111");
 
     return 0;
