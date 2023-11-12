09cbe8c4c1bdfe15a566509131c0c235da562942
Daniel Sucupira Lima <daniellima32@gmail.com>
2023-11-12 19:06:56 -0300

    Feitos 7 ex de q 2

--- PA/main.cpp
+++ PA/main.cpp
@@ -9,8 +9,8 @@ using namespace std;
 int main()
 {
     //Example 1
-    AutomataExecution automataExecution(PushdownAutomataSamples::get_structure_of_sample_1());
-    automataExecution.draw_automata_0p();
+    //AutomataExecution automataExecution(PushdownAutomataSamples::get_structure_of_sample_1());
+    //automataExecution.draw_automata_0p();
 
     //automataExecution.process_word("01");
     //automataExecution.process_word("0011");
@@ -18,13 +18,19 @@ int main()
     //automataExecution.process_word("0");
     //automataExecution.process_word("1");
     //automataExecution.process_word("001");
-    automataExecution.process_word("011");
+    //automataExecution.process_word("011");
 
     //Example 2
-    //AutomataExecution automataExecution(PushdownAutomataSamples::get_structure_of_sample_2());
-    //automataExecution.draw_automata_0p();
+    AutomataExecution automataExecution(PushdownAutomataSamples::get_structure_of_sample_2());
+    automataExecution.draw_automata_0p();
 
     //automataExecution.process_word("00");
+    //automataExecution.process_word("0110");
+    //automataExecution.process_word("010010");
+    //automataExecution.process_word("01");
+    //automataExecution.process_word("001");
+    //automataExecution.process_word("010");
+    automataExecution.process_word("0101");
 
     //Example 4
     //AutomataExecution automataExecution(PushdownAutomataSamples::get_structure_of_sample_4());
